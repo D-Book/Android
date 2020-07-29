81b2b61b79fe80f35abe8dee16f1cf99879e9349
bjj3036 <baebo0823@naver.com>
2020-07-29 21:32:01 +0900

    [OPTIMIZE] 프로젝트 정리

---
+++ app/src/main/java/kr/hs/dgsw/dbook/bottom_navagation.java
@@ -0,0 +1,16 @@
+package kr.hs.dgsw.dbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class bottom_navagation extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navagation);
+
+    }
+
+}
