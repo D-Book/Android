2b16beb9daf277324d4f6ad7b9584b9386b489f2
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-07-29 21:02:34 +0900

    20200729
    권한설정

--- practice_navigation/app/src/main/java/com/example/practice_navigation/bottom_navagation.java
+++ practice_navigation/app/src/main/java/com/example/practice_navigation/bottom_navagation.java
@@ -5,19 +5,13 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import java.util.ArrayList;
 
 public class bottom_navagation extends Activity {
 
-    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
