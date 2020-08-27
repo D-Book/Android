fff54849fe7cd3b66ff024d557e46f398d1d071c
baejunjae1009 <baejunjae1009@gmail.com>
2020-08-27 17:38:28 +0900

    [FEATURE] ViewerActivity 대충 생성

---
+++ app/src/main/java/kr/hs/dgsw/dbook/ViewerActivity.kt
@@ -0,0 +1,98 @@
+package kr.hs.dgsw.dbook
+
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.namo.epub.*
+import com.namo.epub.model.EpubAnnotation
+import com.namo.epub.model.PackageDocument
+import com.namo.epub.model.SearchResult
+import com.namo.epub.model.State
+import com.namo.epub.model.attr.RenditionLayout
+import kr.hs.dgsw.dbook.namo.DefaultEpubDRM
+import kr.hs.dgsw.dbook.namo.DefaultViewerSettings
+
+class ViewerActivity : AppCompatActivity(), EpubManager.Listener {
+
+    lateinit var mEpubManager: EpubManager
+    lateinit var mSettings: EpubSettings
+    lateinit var epubProvider: EpubProvider
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setEpubManager()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mEpubManager.destroy()
+        epubProvider.destroy()
+    }
+
+    fun setEpubManager() {
+        mSettings = DefaultViewerSettings()
+        val epubDRM = DefaultEpubDRM("")
+        epubProvider = EpubFactory.createProvider(this, epubDRM, mSettings)
+        epubProvider.run()
+        mEpubManager = EpubFactory.createManager(this, "", "", epubProvider, mSettings)
+    }
+
+    override fun onParseCompleted() {
+        mEpubManager.open(null,  null)
+    }
+
+    override fun onMessage(p0: EpubManager?, message: Int) {
+    }
+
+    override fun onException(p0: EpubManager?, p1: EpubException?) {
+    }
+
+    override fun onOpened(p0: EpubManager?) {
+    }
+
+    override fun onPageChangeRequested(p0: EpubManager?, p1: PackageDocument.Spine.Itemref?, p2: State?): Boolean = false
+
+    override fun onPageChanged(p0: EpubManager?, p1: Int, p2: Int, p3: Int, p4: RenditionLayout?) {
+    }
+
+    override fun onScaleChanged(p0: EpubManager?, p1: Float, p2: Float) {
+    }
+
+    override fun onPageCalculationStart(p0: EpubManager?) {
+    }
+
+    override fun onPageCalculationProgress(p0: EpubManager?, p1: Int, p2: Int) {
+    }
+
+    override fun onPageCalculationEnd(p0: EpubManager?, p1: Int, p2: Boolean) {
+    }
+
+    override fun onSearchStart(p0: EpubManager?) {
+    }
+
+    override fun onSearchProgress(p0: EpubManager?, p1: Int, p2: Int, p3: MutableList<SearchResult>?) {
+    }
+
+    override fun onSearchStopped(p0: EpubManager?, p1: Int, p2: Int, p3: MutableList<SearchResult>?) {
+    }
+
+    override fun onSearchEnd(p0: EpubManager?) {
+    }
+
+    override fun onFling(p0: EpubManager?, p1: MotionEvent?, p2: MotionEvent?, p3: Float, p4: Float): Boolean = false
+
+    override fun onClick(p0: EpubManager?, p1: Int, p2: Int, p3: Int, p4: Int, p5: EpubSettings.AreaType?): Boolean = false
+
+    override fun onLinkClick(p0: EpubManager?, p1: String?, p2: Boolean) {
+    }
+
+    override fun onNoterefClick(p0: EpubManager?, p1: String?, p2: String?, p3: Rect?) {
+    }
+
+    override fun onAnnotationSelected(p0: EpubManager?, p1: EpubAnnotation?, p2: Rect?, p3: Rect?, p4: EpubSettings.ViewDirection?) {
+    }
+
+    override fun onSelectionCleared(p0: EpubManager?) {
+    }
+}
