029e60ef68881780ca463411d87f29b976932f45
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-10-28 20:58:29 +0900

    [UPDATE]

--- app/src/main/java/kr/hs/dgsw/dbook/ui/fragment/BookListFragment.kt
+++ app/src/main/java/kr/hs/dgsw/dbook/ui/fragment/BookListFragment.kt
@@ -11,7 +11,10 @@ class BookListFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
-            savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_book_list, container, false)
+
+        return view
     }
 }
