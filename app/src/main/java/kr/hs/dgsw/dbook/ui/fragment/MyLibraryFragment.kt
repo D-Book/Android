4ff1751f3655c809f04d1d309af7b76a90c411b6
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2020-11-09 22:09:42 +0900

    [Development]MyLibraryFragment

--- app/src/main/java/kr/hs/dgsw/dbook/ui/fragment/MyLibraryFragment.kt
+++ app/src/main/java/kr/hs/dgsw/dbook/ui/fragment/MyLibraryFragment.kt
@@ -1,19 +1,29 @@
 package kr.hs.dgsw.dbook.ui.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import kr.hs.dgsw.dbook.MyAdapter
 import kr.hs.dgsw.dbook.R
 import kr.hs.dgsw.dbook.model.EBookModel
 
-class MyLibraryFragment : Fragment(R.layout.fragment_my_library) {
+class MyLibraryFragment : Fragment() {
     var bookList = arrayListOf<EBookModel>()
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+            View? {
+        val view = inflater.inflate(R.layout.fragment_book_list, container, false)
         val mAdapter = MyAdapter(requireContext(), bookList)
-        val DRV : RecyclerView = view.findViewById(R.id.downloadRecycler)
+        val DRV: RecyclerView = view.findViewById(R.id.downloadRecycler)
         DRV.adapter = mAdapter
+        //                Glide.with(context)
+//                    .load(profile.image_urls[i+1])
+//                    .into(userProfile2)
+        return view
+
     }
+
+
 }
